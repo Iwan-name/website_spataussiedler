3870d6fcb6fbf007969070d5a15f2032fa29fe19
Ivan <dearivan.vasilenko@yandex.ru>
2024-05-05 21:02:24 +0300

    профиль в доработке

--- spataussiedler/user_profile/urls.py
+++ spataussiedler/user_profile/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-path('profile/', views.profile, name='profile'),
+urlpatterns = [
+    path('', views.profile, name='profile'),
+    path('add_unterlagen/', views.add_unterlagen, name='add_unterlagen'),
+    path('add_prufung/', views.add_prufung, name='add_prufung'),
+    path('add_interessante/', views.add_interessante, name='add_interessante'),
+    path('add_umzug/', views.add_umzug, name='add_umzug'),
+
+]
--- spataussiedler/user_profile/views.py
+++ spataussiedler/user_profile/views.py
@@ -3,7 +3,43 @@ from django.shortcuts import render
 
 def profile(request):
     """ Функия прфиля пользывателя """
-    template = 'users/profile.html'
+    template = 'user_profile/user_profile.html'
+    context = {
+
+    }
+    return render(request, template)
+
+
+def add_unterlagen(request):
+    """ Функция добавления поста про документы """
+    template = 'user_profile/add_unterlagen.html'
+    context = {
+
+    }
+    return render(request, template)
+
+
+def add_prufung(request):
+    """ Функция добавления поста про экзамен """
+    template = 'user_profile/add_prufung.html'
+    context = {
+
+    }
+    return render(request, template)
+
+
+def add_interessante(request):
+    """ Функция добавления поста про интересные места """
+    template = 'user_profile/add_interessante.html'
+    context = {
+
+    }
+    return render(request, template)
+
+
+def add_umzug(request):
+    """ Функция добавления поста про переезд """
+    template = 'user_profile/add_umzug.html'
     context = {
 
     }
