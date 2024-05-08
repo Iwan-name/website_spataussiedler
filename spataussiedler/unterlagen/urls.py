db4509d106c0758cd2f22dd33766867d9c044956
Iwan-name <dearivan.vasilenko@yandex.ru>
2024-05-08 14:32:57 +0300

    download

--- spataussiedler/unterlagen/urls.py
+++ spataussiedler/unterlagen/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import download_file
 
 urlpatterns = [
     path('/posts', views.unterlagen, name='unterlagen'),
-    path('/posts/<int:post_id>/', views.unterlagen_post_detail, name='unterlagen_post_detail')
+    path('/posts/<int:post_id>/', views.unterlagen_post_detail, name='unterlagen_post_detail'),
+    path('/download/<str:file_path>/', download_file, name='download_file'),
 ]
--- spataussiedler/unterlagen/views.py
+++ spataussiedler/unterlagen/views.py
@@ -1,10 +1,14 @@
+import os
+
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from spataussiedler.settings import MEDIA_ROOT
 
 from .models import UnterlagenModel
 
 
 def unterlagen(request):
-    """ Функция для страницы unterlagen(посты про документы) """
+    """ Представление для страницы unterlagen(посты про документы) """
     template = 'unterlagen/unterlagen.html'
     posts = UnterlagenModel.objects.all()
     context = {
@@ -14,8 +18,23 @@ def unterlagen(request):
 
 
 def unterlagen_post_detail(request, post_id):
+    """ Представление для отдельного поста про документы """
     post = get_object_or_404(UnterlagenModel, id=post_id)
     context = {
         'post': post,
     }
     return render(request, 'unterlagen/unterlagen_post_detail.html', context)
+
+
+def download_file(request, file_path):
+    """ Функция для скачивания файлов из поста """
+    print(1)
+    file_path = os.path.join(MEDIA_ROOT, file_path)
+    print('sss', file_path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            response = FileResponse(file.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)
+            return response
+    else:
+        return HttpResponse('Файла нет', status=404)
